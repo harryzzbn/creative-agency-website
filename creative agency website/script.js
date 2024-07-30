851b2698b85f69e6d5a142927b6ff6f804b601aa
harryzzbn <154023906+harryzzbn@users.noreply.github.com>
2024-07-30 06:45:28 +0200

    Update script.js

--- creative agency website/script.js
+++ creative agency website/script.js
@@ -9,4 +9,60 @@ toggleButton.addEventListener("click", () => {
 navigation.addEventListener("click", () => {
   toggleButton.classList.toggle("active");
   navigation.classList.toggle("active");
+});toggleButton.addEventListener("click", () => {
+  toggleButton.classList.toggle("active");
+  navigation.classList.toggle("active");
 });
+
+navigation.addEventListener("click", () => {
+  toggleButton.classList.toggle("active");
+  navigation.classList.toggle("active");
+});toggleButton.addEventListener("click", () => {
+  toggleButton.classList.toggle("active");
+  navigation.classList.toggle("active");
+});
+
+navigation.addEventListener("click", () => {
+  toggleButton.classList.toggle("active");
+  navigation.classList.toggle("active");
+});toggleButton.addEventListener("click", () => {
+  toggleButton.classList.toggle("active");
+  navigation.classList.toggle("active");
+});
+
+navigation.addEventListener("click", () => {
+  toggleButton.classList.toggle("active");
+  navigation.classList.toggle("active");
+});toggleButton.addEventListener("click", () => {
+  toggleButton.classList.toggle("active");
+  navigation.classList.toggle("active");
+});
+
+navigation.addEventListener("click", () => {
+  toggleButton.classList.toggle("active");
+  navigation.classList.toggle("active");
+});toggleButton.addEventListener("click", () => {
+  toggleButton.classList.toggle("active");
+  navigation.classList.toggle("active");
+});
+
+navigation.addEventListener("click", () => {
+  toggleButton.classList.toggle("active");
+  navigation.classList.toggle("active");
+});toggleButton.addEventListener("click", () => {
+  toggleButton.classList.toggle("active");
+  navigation.classList.toggle("active");
+});
+
+navigation.addEventListener("click", () => {
+  toggleButton.classList.toggle("active");
+  navigation.classList.toggle("active");
+});toggleButton.addEventListener("click", () => {
+  toggleButton.classList.toggle("active");
+  navigation.classList.toggle("active");
+});
+
+navigation.addEventListener("click", () => {
+  toggleButton.classList.toggle("active");
+  navigation.classList.toggle("active");
+});v
